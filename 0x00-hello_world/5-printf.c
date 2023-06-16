1aa3ff8bf9cefc25ab564168235b034fd6a2af67
“Ewura-Esi” <meaadjei@gmail.com>
2023-06-16 12:50:49 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This is the main function that prints a specific message
+ * and returns 0 to indicate successful execution.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

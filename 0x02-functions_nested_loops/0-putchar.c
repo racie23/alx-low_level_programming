ff10f876653681dff432d6c42b17d568437982a9
“Ewura-Esi” <meaadjei@gmail.com>
2023-06-20 15:00:16 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,20 @@
 #include "main.h"
-
 /**
- * main - Entry point of the program
- *
- * Description: This function prints the string "_putchar"
- *              followed by a new line using putchar.
- *
- * Return: Always 0 (Success)
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
+
 int main(void)
 {
-	int i;
-	char str[] = "_putchar\n";
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,22 +1,6 @@
-#include "stdio.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - Entry point of the program
- *
- * Description: This function prints the string "_putchar"
- *              followed by a new line using putchar.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int i;
-	char str[] = "_putchar\n";
+int _putchar(char c);
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
-
-	return (0);
-}
+#endif /* MAIN_H */

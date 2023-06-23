149ccf8a17555fca34a3ac60eb896fc18018cf7c
“Ewura-Esi” <meaadjei@gmail.com>
2023-06-23 07:36:10 -0700

    isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * _isupper - Check if a letter is uppercase
- * @x: The character to be checked
- *
- * Return: 1 if @x is uppercase, 0 otherwise
+ * _isupper - Check if a letter is upper
+ * @x: The number to be checked
+ * Return: 1 for upper letter or 0 for any else
  */
+
 int _isupper(int x)
+
 {
+
 	if (x >= 65 && x <= 90)
 	{
-		return (1);
+	return (1);
 	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-int_putchar(char c);
+int _putchar(char c);
 
 int _isupper(int c);
 
@@ -19,3 +19,5 @@ void print_diagonal(int n);
 void print_square(int size);
 
 void print_triangle(int size);
+
+void print_number(int n);

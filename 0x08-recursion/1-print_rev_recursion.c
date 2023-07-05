500f82faf4459f98257f1c2477e78b374db36eb1
“Ewura-Esi” <meaadjei@gmail.com>
2023-07-05 15:20:41 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion: Prints a string in reverse
- * 
+ * _print_rev_recursion- Prints a string in reverse
  * @s : The string to print
  *
  * Return: Always 0 (Success)
  */
- 
- void _print_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{

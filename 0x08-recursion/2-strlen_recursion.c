fca21c5982fd430eac9b8296f235e979a9814319
“Ewura-Esi” <meaadjei@gmail.com>
2023-07-05 15:29:15 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string
+ *
+ * @s : The string to return
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	s++;
+	return (_strlen_recursion(s) + 1);
+}

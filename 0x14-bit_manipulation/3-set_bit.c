652014e4b0595065dd52e3927a106da5051cdd8e
“Ewura-Esi” <meaadjei@gmail.com>
2023-08-03 16:14:51 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,12 +2,12 @@
 
 /**
  * set_bit - set a bit at a given index
- * @a: the address to the number to change 
+ * @n: the address to the number to change
  * @index: the index of the bit to set
  *
  * Return: 1 if it worked, or -1 if an error occurred
  */
-int set_bit(unsigned long int *a, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
@@ -16,7 +16,7 @@ int set_bit(unsigned long int *a, unsigned int index)
 		if (index > 63)
 			return (-1);
 
-		*a = ((1UL << index) | *a);
+		*n = ((1UL << index) | *n);
 	}
 
 	return (1);

2e13816a522e2b99c57a3071e571cb010832939d
“Ewura-Esi” <meaadjei@gmail.com>
2023-08-07 08:42:17 -0700

    0x15

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,12 +13,12 @@ void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can not read from file %s\n", agrv[1]);
+		dprintf(STDERR_FILENO, "Error: Can not read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can not write to %s\n", agrv[2]);
+		dprintf(STDERR_FILENO, "Error: Can not write to %s\n", argv[2]);
 		exit(99);
 	}
 }

51f90591c934496405cc33470d5150e59bef3b0c
“Ewura-Esi” <meaadjei@gmail.com>
2023-08-08 08:22:21 -0700

    3-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,14 +1,47 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+
+void error_file(int file_from, int file_to, char *argv[]);
+void copy_files(int file_from, int file_to, char *argv[]);
+void close_files(int file_from, int file_to);
 
 /**
- * error_file - check whether files can be opened
- * @file_from : ...
- * @file_to : ...
- * @argv: the argument vector
- * Return: does not return
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ * Return: Always 0 (success)
  */
+
+int main(int argc, char *argv[])
+{
+	int file_from, file_to;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	error_file(file_from, file_to, argv);
+
+	copy_files(file_from, file_to, argv);
+	close_files(file_from, file_to);
+
+	return (0);
+}
+
+/**
+ * error_file- Check whether files can be opened
+ * @file_from: File descriptor for the source file
+ * @file_to: File descriptor for the destination file
+ * @argv: The argument vector
+ * Return: Does not return
+ */
+
 void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
@@ -24,37 +57,43 @@ void error_file(int file_from, int file_to, char *argv[])
 }
 
 /**
- * main - Check the code
- * @argc: no. of arguments
- * @agrv: the argument vector
- * Return: Always 0 (success)
+ * copy_files - Copy data from source file to destination file
+ * @file_from: File descriptor for the source file
+ * @file_to: File descriptor for the destination file
+ * @argv: The argument vector
+ * Return: Does not return
  */
-int main(int argc, char *agrv[])
+
+void copy_files(int file_from, int file_to, char *argv[])
+
 {
-	int file_from, file_to, err_close;
 	ssize_t nchars, nwr;
 	char buf[1024];
 
-	if (argc != 3)
+	while ((nchars = read(file_from, buf, sizeof(buf))) > 0)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-		exit(97);
-	}
-
-	file_from = open(agrv[1], O_RDONLY);
-	file_to = open(agrv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, agrv);
-
-	nchars = 1024;
-	while (nchars == 1024)
-	{
-		nchars = read(file_from, buf, 1024);
-		if (nchars == -1)
-			error_file(-1, 0, agrv);
 		nwr = write(file_to, buf, nchars);
 		if (nwr == -1)
-			error_file(0, -1, agrv);
+		{
+			error_file(0, -1, argv);
+		}
 	}
+	if (nchars == -1)
+	{
+		error_file(-1, 0, argv);
+	}
+}
+
+/**
+ * close_files - Close file descriptors
+ * @file_from: File descriptor for the source file
+ * @file_to: File descriptor for the destination file
+ * Return: Does not return
+ */
+
+void close_files(int file_from, int file_to)
+{
+	int err_close;
 
 	err_close = close(file_from);
 	if (err_close == -1)
@@ -66,9 +105,8 @@ int main(int argc, char *agrv[])
 	err_close = close(file_to);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can not close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can not close fd %d\n", file_to);
 		exit(100);
 	}
-
-	return (0);
 }
+

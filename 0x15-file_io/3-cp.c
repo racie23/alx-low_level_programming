5680b83f7da591e80c3a17e7f150c3f7cc7ac4f3
“Ewura-Esi” <meaadjei@gmail.com>
2023-09-04 11:39:08 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,125 +1,112 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
-void error_file(int file_from, int file_to);
-void copy_files(int file_from, int file_to);
-void close_files(int file_from, int file_to);
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
- * main - Entry point of the program
- * @argc: Number of command-line arguments
- * @argv: Array of command-line argument strings
- * Return: Always 0 (success)
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
  */
+char *create_buffer(char *file)
+{
+	char *buffer;
 
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
+}
+
+/**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+	int c;
+
+	c = close(fd);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
+ */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to;
+	int from, to, r, w;
+	char *buffer;
 
 	if (argc != 3)
 	{
-		fprintf(stderr, "Usage:%s file_from file_to\n", argv[0]);
-		exit(1);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
-	{
-		perror("Error opening source file");
-		exit(2);
-	}
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC,
-			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-	if (file_to == -1)
-	{
-		perror("Error opening destination file");
-		exit(3);
-	}
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
 
-	copy_files(file_from, file_to);
-	close_files(file_from, file_to);
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buffer);
+	close_file(from);
+	close_file(to);
 
 	return (0);
 }
 
-/**
- * error_file- Check whether files can be opened
- * @file_from: File descriptor for the source file
- * @file_to: File descriptor for the destination file
- * Return: Does not return
- */
 
-void error_file(int file_from, int file_to)
-{
-	if (file_from == -1)
-	{
-	perror("Error reading from source file");
-		exit(4);
-	}
-	if (file_to == -1)
-	{
-		perror("Error writing to destination file");
-		exit(5);
-	}
-}
 
-/**
- * copy_files - Copy data from source file to destination file
- * @file_from: File descriptor for the source file
- * @file_to: File descriptor for the destination file
- * Return: Does not return
- */
 
-void copy_files(int file_from, int file_to)
 
-{
-	ssize_t nchars, nwr;
-	char buf[1024];
 
-	while ((nchars = read(file_from, buf, sizeof(buf))) > 0)
-	{
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
-		{
-			perror("Error writing to destination file");
-			exit(6);
-		}
-	}
-	if (nchars == -1)
-	{
-		perror("Error reading from source file");
-		exit(7);
-	}
-}
 
-/**
- * close_files - Close file descriptors
- * @file_from: File descriptor for the source file
- * @file_to: File descriptor for the destination file
- * Return: Does not return
- */
 
-void close_files(int file_from, int file_to)
-{
-	int err_close;
-
-	err_close = close(file_from);
-	if (err_close == -1)
-	{
-		perror("Error closing source file");
-		exit(8);
-	}
-
-	err_close = close(file_to);
-	if (err_close == -1)
-	{
-		perror("Error closing destination file");
-		exit(9);
-	}
-}
 
